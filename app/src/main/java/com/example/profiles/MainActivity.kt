880a148a1c81cd602f42f92b31719474534d204e
John Barbosa <djbarbosa@uninorte.edu.co>
2020-02-11 19:07:42 -0500

    Fixing details from the template

--- app/src/main/java/com/example/profiles/MainActivity.kt
+++ app/src/main/java/com/example/profiles/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.main_activity_fragment, PlacesFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.main_activity_fragment, profiles()).commit()
     }
 
     fun replaceFragment(fragment: Fragment) {
